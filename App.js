cd5f5dc52add948dd9d4e7d626036c813bacdd14
Ata <106282747+atakanberkyilmaz@users.noreply.github.com>
2023-08-21 12:33:20 +0300

    Harita kullanımı

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View,Text } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  setTimeout(() => {
+    setLocation({
+      latitude: 39.9032923,
+      longitude: 32.6226813
+    });
+  })
+
+  if(errorMsg){
+    return <Text>{errorMsg}</Text>
+  }
+  console.log(location);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      {location && (
+
+     <MapView 
+     style={styles.map}
+      region={{
+        latitude: location?.latitude,
+        longitude: location?.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      
+        
+      }} 
+      />
+    
+  )}
+  </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
+
